a2f55f489decc9576cbda7185291ec8f546bc573
Jelle Kralt <jelle@jellekralt.nl>
2015-04-20 09:55:02 +0200

    Added package.json editing

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var slug = require('slug');
 
 module.exports = yeoman.generators.Base.extend({
 	initializing: function () {
@@ -17,11 +18,39 @@ module.exports = yeoman.generators.Base.extend({
 		));
 
 		var prompts = [{
+			name: 'projectName',
+			message: 'Please the project\'s name'
+		},
+		{
 			type: 'confirm',
-			name: 'someOption',
-			message: 'Would you like to enable this option?',
+			name: 'isPrivate',
+			message: 'Is this a private project?',
 			default: true
-		}];
+		}/*,
+		{
+			type: 'confirm',
+			name: 'addFramework',
+			message: 'Would you like to add a front end framework?',
+			default: false
+		},
+		{
+			type: 'checkbox',
+			name: 'framework',
+			message: 'Which framework would you like to include?',
+			when: function (props) {
+				return props.addFramework;
+			},
+			choices: [
+				{
+					name: 'Angular',
+					value: 'angular'
+				},
+				{
+					name: 'Backbone',
+					value: 'Backbone'
+				}
+			]
+		}*/];
 
 		this.prompt(prompts, function (props) {
 			this.props = props;
@@ -31,22 +60,46 @@ module.exports = yeoman.generators.Base.extend({
 		}.bind(this));
 	},
 
-	writing: function() {
-		this.files = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
+	config: function() {
+		this.config.set('projectName', this.props.projectName);
 
-		var ignores = [
-			'.git',
-			'LICENSE',
-			'README.md',
-		];
+        this.config.save();
+	},
 
-		this.files.forEach(function(file) {
-			if (ignores.indexOf(file) !== -1) {
-				return;
+	writing: {
+
+		copy: function() {
+			this.files = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
+
+			var ignores = [
+				'.git',
+				'LICENSE',
+				'README.md'
+			];
+
+			this.files.forEach(function(file) {
+				if (ignores.indexOf(file) !== -1) {
+					return;
+				}
+
+				this.copy(file, file);
+			}, this);   
+		},
+		parse: function () {
+
+			// Read package data
+			var packageData = this.fs.readJSON('package.json');
+
+			// Overwrite package name with project name
+			packageData.name = slug(this.props.projectName, { lower: true });
+			packageData.description = this.props.projectName;
+
+			if(this.props.isPrivate) {
+				packageData.private = true;
 			}
 
-			this.copy(file, file);
-		}, this);   
+			this.fs.writeJSON('package.json', packageData);
+		}
 	},
 
 	install: function () {
